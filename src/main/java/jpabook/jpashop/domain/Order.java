77530aefcf6a12dc520ed23e750714ee51cebf71
yerim94 <721yerim@gmail.com>
2020-06-19 00:40:07 +0900

    create relation mapping method

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -31,14 +32,25 @@ public class Order {
 	@JoinColumn(name = "member_id")
 	private Member member;
 	
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 	
-	@OneToOne(fetch = FetchType.LAZY)
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 	
 	private LocalTime orderDate; //주문시간 
 	
 	private OrderStatus status; //주문상태 [ORDER, CANCEL]
+	
+	//연관관계 메서드//
+	public void setMember(Member member) {
+		this.member = member;
+		member.getOrders().add(this);
+	}
+	
+	public void setOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
 }
