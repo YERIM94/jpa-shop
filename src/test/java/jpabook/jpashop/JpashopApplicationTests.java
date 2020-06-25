a3724cffdb7cbceae655bbaa4af44b4c55eca809
yerim94 <721yerim@gmail.com>
2020-06-25 23:55:22 +0900

    create memberJoin test code

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,4 +1,4 @@
-package jpabook;
+package jpabook.jpashop;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.service;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@Autowired
+	private MemberRepository  memberRepository;
+	
+	@Autowired
+	private EntityManager em;
+	
+	@Test
+	/* @Rollback(false) //spring에서 test code에 transactional이 있으면 실행 후 롤백시킴. 이를 방지하기 위한 것. */
+	public void 회원가입() throws Exception{
+		//given(주어졌을때)
+		Member member = new Member();
+		member.setName("kim");
+		
+		//when(실행하면)
+		Long savedId = memberService.join(member);
+		
+		//then(이렇게됨)
+		em.flush(); //영속성 context 안에 있는 내용을 DB에 반영(Transactional 어노테이션 있으므로 반영 후 다시 롤백됨) 
+		assertEquals(member, memberRepository.findOne(savedId));
+	}
+	
+	@Test
+	public void 중복_회원_예외() {
+		
+	}
+
+}
