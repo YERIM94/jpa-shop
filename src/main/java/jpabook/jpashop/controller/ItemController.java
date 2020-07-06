e51979422d128e3ff3ae6aa2576be8d450fedea2
yerim94 <721yerim@gmail.com>
2020-07-07 00:08:09 +0900

    create item inquiry function

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,11 +1,14 @@
 package jpabook.jpashop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 
@@ -32,5 +35,12 @@ public class ItemController {
 		
 		itemService.saveItem(book);
 		return "redirect:/";
+	}
+	
+	@GetMapping("/items")
+	public String list(Model model) {
+		List<Item> books = itemService.findItems();
+		model.addAttribute("items", books);
+		return "items/itemList";
 	}
 }
