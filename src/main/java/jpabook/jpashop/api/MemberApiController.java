28193329ad772096e4abcaa50785e2b5ba192242
yerim94 <721yerim@gmail.com>
2020-07-27 22:57:23 +0900

    create findMembers API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,7 +1,11 @@
 package jpabook.jpashop.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +24,40 @@ public class MemberApiController {
 
 	private final MemberService memberService;
 
+	@GetMapping("/api/v1/members")
+	public List<Member> membersV1(){
+		return memberService.findMembers();
+	}
+	
+	@GetMapping("/api/v2/members")
+	public Result membersV2(){
+		List<Member> findMembers = memberService.findMembers();
+		
+		//ver1
+		List<MemberDto> resultMembers = new ArrayList<MemberDto>();
+		
+		for(int i=0; i<findMembers.size();i++) {
+			resultMembers.add(new MemberDto(findMembers.get(i).getName()));
+		}
+		
+		//ver2
+		//List<MemberDto> collect = findMembers.stream().map(m->new MemberDto(m.getName())).collect(Collectors.toList());
+		
+		return new Result(resultMembers);
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class Result<T>{
+		private T data;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class MemberDto{
+		private String name;
+	}
+	
 	@PostMapping("/api/v1/members")
 	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
 		Long id = memberService.join(member);
