a813ab2bc3d2a90f2e4994e75f760c5b18501ae1
yerim94 <721yerim@gmail.com>
2020-11-17 23:58:01 +0900

    modification

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+	
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
 	}
 	
 }
